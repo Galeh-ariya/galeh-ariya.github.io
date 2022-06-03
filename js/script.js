d853a30d07c93713636cdf311505b6d20e0e0f00
Galeh Ariya Irwana <galehariya8@gmail.com>
2022-06-03 17:04:14 +0700

    Memperbaiki gallery menggunakan js

--- js/script.js
+++ js/script.js
@@ -3,23 +3,49 @@ $(window).scroll(function() {
 
     // console.log(wScroll);
 
-    if(wScroll >= 440) {
-		$('.imgs2').css('opacity', '1');
-	} else {
-        $('.imgs2').css('opacity', '0');
+    // gallery
+    if(wScroll >= 1300) {
+		  $('.imgs2').css('opacity', '1');
+	  } else {
+      $('.imgs2').css('opacity', '0');
     }
 
-    if(wScroll >= 1140) {
-		$('.desc').css('opacity', '1');
-		$('.imgs3').css('opacity', '1');
-	} else {
-        $('.desc').css('opacity', '0');
-        $('.imgs3').css('opacity', '0');
+    // course
+    if(wScroll >= 2100) {
+		  $('.desc').css('opacity', '1');
+		  $('.imgs3').css('opacity', '1');
+	  } else {
+      $('.desc').css('opacity', '0');
+      $('.imgs3').css('opacity', '0');
     }
 
-    if(wScroll >= 2050) {
-		$('.icon').css('opacity', '1');
-	} else {
-        $('.icon').css('opacity', '0');
+    // social media
+    if(wScroll >= 3000) {
+		  $('.icon').css('opacity', '1');
+	  } else {
+      $('.icon').css('opacity', '0');
     }
+
+    // skill
+    if(wScroll >= 580) {
+		  $('.jav').css('opacity', '1');
+		  $('.html').css('opacity', '1');
+		  $('.css').css('opacity', '1');
+		  $('.js').css('opacity', '1');
+	  } else {
+      $('.jav').css('opacity', '0');
+      $('.html').css('opacity', '0');
+      $('.css').css('opacity', '0');
+      $('.js').css('opacity', '0');
+    }
+
 })
+
+// Gallery
+for(let i = 1; i <= 7; i++) {
+  $('#img').append(`
+    <div class="cards">
+      <div class="card"><img src="img/pap`+ i +`.jpg" width="100%"></div>
+    </div>
+  `);
+}
