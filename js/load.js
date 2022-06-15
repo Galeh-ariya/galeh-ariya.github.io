36598370af89a89a2ce882266d82b746c12f936b
Galeh Ariya Irwana <galehariya8@gmail.com>
2022-06-15 22:52:21 +0700

    Update loader v2

--- js/load.js
+++ js/load.js
@@ -1,7 +1,16 @@
+// $(document).ready(function() {
+//     const myTimeOut = setTimeout(preload, 3000);
+
+//    function preload() {
+//     $('#load').remove('.load');
+//    }
+// });
+
 $(document).ready(function() {
     const myTimeOut = setTimeout(preload, 3000);
 
    function preload() {
-    $('#load').remove('.load');
+    const load = document.getElementById('container');
+    load.style.display = 'none';
    }
 });
